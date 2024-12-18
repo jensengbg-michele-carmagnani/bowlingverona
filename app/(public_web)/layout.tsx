f4865513f8a3c1874637cc234cccdb6f084da5a4
Karma <michele.carmagnani@gmail.com>
2024-12-18 19:08:29 +0100

    fix: favicon

--- app/(public_web)/layout.tsx
+++ app/(public_web)/layout.tsx
@@ -67,9 +67,9 @@ export const metadata: Metadata = {
     "Bowling Verona, piste da bowling, divertimento in famiglia, leghe di bowling, feste di bowling, intrattenimento a Verona",
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon.ico", sizes: "any" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
   },
 };
