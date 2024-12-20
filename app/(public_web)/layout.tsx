d4754e5a3367842a6806e2198768f348930e7dfe
Karma <michele.carmagnani@gmail.com>
2024-12-20 02:18:22 +0100

    feat: improve seo

--- app/(public_web)/layout.tsx
+++ app/(public_web)/layout.tsx
@@ -60,11 +60,27 @@ const Rajdhani = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Bowling Verona - Esperienza di Bowling Premium a Verona",
+  title: "Bowling Verona - Divertimento a Verona - Bowling Verona",
   description:
-    "Scopri il divertimento definitivo al Bowling Verona! Perfetto per famiglie, amici ed eventi. Unisciti alle nostre leghe o organizza la tua prossima festa con noi. Prenota il tuo lane oggi!",
+    "Bowling Verona è la destinazione ideale per famiglie e amici. Gioca a bowling, organizza feste, o partecipa a eventi esclusivi. Prenota subito!",
   keywords:
-    "Bowling Verona, piste da bowling, divertimento in famiglia, leghe di bowling, feste di bowling, intrattenimento a Verona",
+    "Bowling Verona, intrattenimento Verona, feste di bowling, eventi aziendali, piste di bowling",
+  openGraph: {
+    title: "Bowling Verona",
+    description:
+      "La migliore esperienza di bowling a Verona. Ideale per famiglie, amici e team building.",
+    url: "https://www.bowlingveron.com",
+    images: [
+      {
+        url: "/og-image.png", // Add an Open Graph image in the `public` folder
+        width: 1200,
+        height: 630,
+        alt: "Bowling Verona - Esperienza unica",
+      },
+    ],
+    siteName: "Bowling Verona",
+    type: "website",
+  },
   icons: {
     icon: [
       { url: "/favicon/favicon.ico", sizes: "any" },
@@ -87,13 +103,40 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="it" className={`${Rajdhani.className} ${DomCasualD.className}`}>
       <Script
-        id="Cookiebot"
-        src="https://consent.cookiebot.com/uc.js"
-        data-cbid="47dbaf39-577a-45e6-9ea1-d36a3746754b"
-        type="text/javascript"
+        id="structured-data"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "LocalBusiness",
+            name: "Bowling Verona",
+            description:
+              "Bowling Verona è il punto di riferimento per il divertimento a Verona, con 14 piste da bowling professionali, area giochi, biliardo, e spazi dedicati a feste private ed eventi aziendali. Ideale per famiglie, gruppi di amici e team building. Offriamo un'atmosfera accogliente, giochi moderni e tanto divertimento. Prenota ora per vivere un'esperienza indimenticabile!",
+            address: {
+              "@type": "PostalAddress",
+              streetAddress: "Viale della Fiera 10/A",
+              addressLocality: "Verona",
+              addressRegion: "VR",
+              postalCode: "37135",
+              addressCountry: "IT",
+            },
+            image: "https://www.bowlingveron.com/og-image.jpg", // Replace with your Open Graph image
+            telephone: "+39 045 581724",
+            openingHours: [
+              "Mo-Su 10:00-23:00", // Adjust your actual opening hours
+            ],
+            url: "https://www.bowlingveron.com",
+            sameAs: [
+              "https://www.instagram.com/bowlingverona",
+              "https://www.facebook.com/BowlingFieraVerona",
+            ],
+            priceRange: "$$", // Adjust the price range to reflect your services
+          }),
+        }}
       />
+
       <body className="w-2xl overflow-x-hidden relative">
-        <Suspense fallback={null}>
+        <Suspense fallback={<div>...loading</div>}>
           <ConfidentialFlagValues values={values} />
         </Suspense>
         <Navbar />
